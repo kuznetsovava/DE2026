be318f6bdd14702fe3678f6bf6d6258fc68880f8
Valeriia Kuznetsova <122671128+kuznetsovava@users.noreply.github.com>
2026-05-03 22:38:46 +0300

    Update insert_dim.sql

--- insert_dim.sql
+++ insert_dim.sql
@@ -1,26 +1,16 @@
 INSERT INTO dwh.dim_date (id, date_dat, date_month, date_quarter, date_year)
 SELECT 
-    -- id: используем целочисленное представление даты (например, 20220101)
     TO_CHAR(d, 'YYYYMMDD')::INT AS id,
-
     d::DATE AS date_dat,
-    
-    -- Первый день месяца
     DATE_TRUNC('month', d)::DATE AS date_month,
-    
-    -- Первый день квартала
     DATE_TRUNC('quarter', d)::DATE AS date_quarter,
-    
-    -- Первый день года
     DATE_TRUNC('year', d)::DATE AS date_year
-    
 FROM generate_series(
     '2022-01-01'::DATE,
-    '2024-12-31'::DATE,
+    '2023-12-31'::DATE,
     '1 day'::INTERVAL
 ) AS s(d)
-ON CONFLICT (id) DO NOTHING; 
-
+ON CONFLICT (id) DO NOTHING;
 
 -- Заполнение dim_city
 INSERT INTO dwh.dim_city (id, ccode, nname) VALUES
